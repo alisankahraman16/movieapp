84ae77059741538f8852dc99e6e52d853b64b7f7
Alisan Kahraman <aakahraman34@gmail.com>
2022-11-09 11:15:57 +0300

    update

--- movieapp/movie/models.py
+++ movieapp/movie/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    audience = models.ForeignKey("aud.User",on_delete = models.CASCADE)
+    title = models.CharField(max_lenght = 50)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add = True)
