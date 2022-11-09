52addfc11cc9b483376c52330e2917db7ba34404
Alisan Kahraman <aakahraman34@gmail.com>
2022-11-09 20:43:35 +0300

    upd

--- movie/movie/urls.py
+++ movie/movie/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Movies.views import index
+from Movies import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index,name="index")
+    path('',views.index,name="index")
 ]
