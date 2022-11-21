0242a223ef75a188ff52d37475d90ed331b03e52
Alisan Kahraman <aakahraman34@gmail.com>
2022-11-21 14:23:48 +0300

    a

--- catalog/pages/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-#localhost:9000
-def index(request):
-    return render(request, 'pages/index.html')
-
-def about(request):
-    return render(request, 'pages/about.html')
