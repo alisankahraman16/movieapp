fef43c6d355fde8a897b7447efe828b4d9b8c61e
Alisan Kahraman <aakahraman34@gmail.com>
2022-12-07 12:47:54 +0300

    değişiklik-deneme

--- movie/film/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Film(models.Model):
-    film_id = models.CharField(max_length=20, primary_key=True)
-    title = models.CharField(max_length=20)
-    keywords = models.CharField(max_length=20)
